ed21a9bd09048a737bbce7932735fee197ff3f56
wagnermarques <wagnerdocri@gmail.com>
2026-04-02 17:08:50 -0300

    add accordeon items in menu drawer

--- angular-ui/src/app/app.ts
+++ angular-ui/src/app/app.ts
@@ -8,6 +8,8 @@ import { MatSidenavModule, MatDrawer } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { ConfigService } from './services/config.service';
 import { JsonPipe } from '@angular/common';
 
@@ -22,6 +24,8 @@ import { JsonPipe } from '@angular/common';
     MatListModule,
     MatIconModule,
     MatDividerModule,
+    MatExpansionModule,
+    MatTooltipModule,
     FixedHead,
     FixedStatusbar,
     JsonPipe,
